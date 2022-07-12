a2de8baf83f6b33f6300d1ac3ca2598e01b059f5
KUMAR SANTOSH <97560663+KuSantosh100@users.noreply.github.com>
2022-07-12 20:56:18 +0530

    Add files via upload

---
+++ t2.q2.py
@@ -0,0 +1,11 @@
+#Task2.Q2.How to use "do-while" in python??
+num=234
+while True:
+    n=int(input())
+    if num!=n:
+        print("Wrong")
+    else:
+        print("True , you got it right")
+        break
+    
+        
